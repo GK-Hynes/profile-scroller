33e2b862ea9bc44f19c55c5437c744865642787c
Gerard Hynes <gkhynes@gmail.com>
2018-07-19 21:57:10 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    name: "Myles na gCopaleen",
+    age: 32,
+    gender: "male",
+    lookingfor: "female",
+    location: "Corca Dorcha",
+    image: "https://randomuser.me/api/portraits/men/34.jpg"
+  },
+  {
+    name: "Peig Sayers",
+    age: 28,
+    gender: "female",
+    lookingfor: "male",
+    location: "Dún Chaoin",
+    image: "https://randomuser.me/api/portraits/women/45.jpg"
+  },
+  {
+    name: "Tomás Ó Criomhthain",
+    age: 29,
+    gender: "male",
+    lookingfor: "female",
+    location: "An Blascaod Mór",
+    image: "https://randomuser.me/api/portraits/men/22.jpg"
+  }
+];
